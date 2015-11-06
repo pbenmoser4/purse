15ef1c05ffb056d4bf8c97a130cc86dba99203a3
Percival Moser <pbenmoser4@gmail.com>
2015-11-06 13:58:19 -0500

    adding tests for Digraph

--- python/test_digraph.py
+++ python/test_digraph.py
@@ -1,36 +1,103 @@
 import unittest
 from exceptions import AttributeError, KeyError
-from digraph import Digraph, DNode, DEdge
+from digraph import Digraph
 
 # class DigraphTestCase(unittest.TestCase):
 #     """
 #     Tests for the Digraph class
 #     """
 
-class DNodeTestCase(unittest.TestCase):
-    """
-    Tests for the digraph DNode class
-    """
+class DigraphTestCase(unittest.TestCase):
 
-    def test_str(self):
-        node = DNode('value')
-        self.assertEqual(str(node), 'value')
+    def test_init(self):
+        """
+        Digraph initialization creates an empty dictionary for .nodes
+        """
+        dgraph = Digraph()
+        self.assertEquals(dgraph.nodes, {})
 
-    def test_eq(self):
-        node = DNode('val')
-        other = DNode('val')
-        self.assertEqual(node, other)
-        self.assertIsNot(node, other)
+    def test_get_nodes(self):
+        """
+        test get_nodes
 
-    def test_ne(self):
-        node = DNode('val')
-        other = DNode('other')
-        self.assertNotEqual(node, other)
+        We will add a bunch of nodes and make sure it returns all of them. We're
+        using a set comparison, because
+        """
 
-    def test_hash(self):
-        node = DNode('val')
-        other = DNode('val')
-        self.assertEqual(hash(node), hash(other))
+        dgraph = Digraph()
+        node_list = ['one', 'two', 'three']
+        for n in node_list:
+            dgraph.add_node(n)
+        self.assertEquals(set(dgraph.get_nodes()), set(node_list))
+
+
+    def test_get_neighbors(self):
+        """
+        test get_neighbors
+
+        Add a few nodes to a graph, and then add a few neighbors to that node,
+        and check whether get_neighbors returns the expected result
+        """
+
+        dgraph = Digraph()
+        node_list = ['one', 'two', 'three']
+        for n in node_list:
+            dgraph.add_node(n)
+            if n != 'one':
+                dgraph.add_edge(('one', n))
+        self.assertEquals(dgraph.get_neighbors('one'), ['two', 'three'])
+
+    def test_get_edges(self):
+        """
+        test get_edges
+
+        Add a few nodes to a graph, and then add a few edges to the graph. Check
+        whether get_edges returns the expected edges
+        """
+
+        dgraph = Digraph()
+        node_list = ['one', 'two', 'three']
+        for n in node_list:
+            dgraph.add_node(n)
+            if n != 'one':
+                dgraph.add_edge(('one', n))
+        edges = [('one', 'two'), ('one', 'three')]
+        self.assertEquals(dgraph.get_edges(), edges)
+
+    def test_has_node(self):
+        """
+        test has_node
+        """
+
+    def test_has_edge(self):
+        """
+        test has_edge
+        """
+
+    def test_add_node(self):
+        """
+        test add_node
+        """
+
+    def test_add_edge(self):
+        """
+        test add_edge
+        """
+
+    def test_del_node(self):
+        """
+        test del_node
+        """
+
+    def test_del_edge(self):
+        """
+        Test del_edge
+        """
+
+    def test_get_node_order(self):
+        """
+        Test get_node_order
+        """
 
 if __name__ == '__main__':
     unittest.main()
