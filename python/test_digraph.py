da1e2341c3016910e4ecc0de65456074c604e6b9
Percival Moser <pbenmoser4@gmail.com>
2015-11-06 14:15:31 -0500

    finished test cases for the Digraph class

--- python/test_digraph.py
+++ python/test_digraph.py
@@ -69,35 +69,83 @@ class DigraphTestCase(unittest.TestCase):
         test has_node
         """
 
+        dgraph = Digraph()
+        dgraph.add_node('one')
+        self.assertTrue(dgraph.has_node('one'))
+
     def test_has_edge(self):
         """
         test has_edge
         """
+
+        dgraph = Digraph()
+        node_list = ['one', 'two', 'three']
+        for n in node_list:
+            dgraph.add_node(n)
+            if n != 'one':
+                dgraph.add_edge(('one', n))
+        self.assertTrue(dgraph.has_edge(('one', 'two')))
+        self.assertTrue(dgraph.has_edge(('one', 'three')))
 
     def test_add_node(self):
         """
         test add_node
         """
 
+        dgraph = Digraph()
+        dgraph.add_node('one')
+        self.assertIn('one', dgraph.nodes)
+
     def test_add_edge(self):
         """
         test add_edge
         """
 
+        dgraph = Digraph()
+        node_list = ['one', 'two', 'three']
+        for n in node_list:
+            dgraph.add_node(n)
+        dgraph.add_edge(('one', 'two'))
+        self.assertIn('two', dgraph.nodes['one'])
+
     def test_del_node(self):
         """
         test del_node
         """
+        dgraph = Digraph()
+        node_list = ['one', 'two', 'three']
+        for n in node_list:
+            dgraph.add_node(n)
+        dgraph.del_node('one')
+        self.assertNotIn('one', dgraph.nodes)
 
     def test_del_edge(self):
         """
         Test del_edge
         """
 
+        dgraph = Digraph()
+        node_list = ['one', 'two', 'three']
+        for n in node_list:
+            dgraph.add_node(n)
+            if n != 'one':
+                dgraph.add_edge(('one', n))
+        dgraph.del_edge(('one', 'two'))
+        self.assertNotIn('two', dgraph.nodes['one'])
+
     def test_get_node_order(self):
         """
         Test get_node_order
         """
+
+        dgraph = Digraph()
+        node_list = ['one', 'two', 'three']
+        for n in node_list:
+            dgraph.add_node(n)
+            if n != 'one':
+                dgraph.add_edge(('one', n))
+        self.assertEquals(dgraph.get_node_order('one'), 2)
+
 
 if __name__ == '__main__':
     unittest.main()
