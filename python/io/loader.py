5f5cc2f334ccb6fb0b72e2ec66f3f45f302d0d6a
Percival Moser <pbenmoser4@gmail.com>
2015-11-08 13:32:55 -0500

    working with file structure again

---
+++ python/io/loader.py
@@ -0,0 +1,25 @@
+class Loader:
+    """
+    A class designed to load data into a graph
+    """
+
+    def __init__(self):
+        """
+        Who knows what
+        """
+
+    def load_text(self, filepath, structure):
+        """
+        Load a text file into the given structure, and then return that struct
+
+        @type   filepath: string
+        @param  filepath: The path to the file that you want to load
+
+        @type   structure: graph, tree, or other structure
+        @param  structure: The structure that you want to load text into
+
+        @rtype  graph, tree, or other structure
+        @return The structure that has been populated
+        """
+
+        
